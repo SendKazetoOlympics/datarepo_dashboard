ea807ac83ac87610779e54aa8562cfcb364eba85
Kaze Wong <kazewong.physics@gmail.com>
2024-08-15 21:22:09 -0400

    update workout page

--- src/routes/workout/+page.svelte
+++ src/routes/workout/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Document you workout here</h1>
+<script>
+    async function getProjectList() {
+        const project_list = await fetch('http://kazeserver:8084/api/projects/', {
+            headers: {
+                'Authorization': 'Token  <api_key>'
+            }
+            });
+        return project_list;
+    }
+    const date = new Date();
+    let project_list = getProjectList();
+</script>
 
+<h1>Document you workout here</h1>
+<p>Today is {date.toDateString()}</p>
