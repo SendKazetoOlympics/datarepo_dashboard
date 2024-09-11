7cf0f231e7f92c93fa708e570e813f5d00fdfb83
Kaze Wong <kazewong.physics@gmail.com>
2024-09-10 21:52:35 -0400

    Add tailwind and daisy ui

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: sequence([vitePreprocess({
+	preprocess: vitePreprocess({
 		typescript: true
-	}), preprocessMeltUI()]),
+	}),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
