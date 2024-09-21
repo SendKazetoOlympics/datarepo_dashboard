c3277c888a0c2fa382e07164e63aed6011ff61de
Kaze Wong <kazewong.physics@gmail.com>
2024-09-20 21:50:00 -0400

    move more server code to flask

--- src/routes/api/database/select_video_by_name/+server.ts
+++
@@ -1,9 +0,0 @@
-import { selectVideoByName } from "$lib/server/database";
-
-export async function POST({ request }) {
-    const formData = await request.formData();
-    const name = formData.get("name") as string;
-    const videos = await selectVideoByName(name);
-    const response = new Response(JSON.stringify(videos));
-    return response;
-}
--- src/routes/api/minio/get_video_url/+server.ts
+++
@@ -1,9 +0,0 @@
-import { getPresignedUrl } from "$lib/server/minio";
-
-export async function POST({ request }) {
-    const formData = await request.formData();
-    const filename = formData.get("objectName") as string;
-    const url = await getPresignedUrl("highjump", filename);
-    const response = new Response(JSON.stringify({ url }));
-    return response;
-}
