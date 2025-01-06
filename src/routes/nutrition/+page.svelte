b79c83249feac484e70ed00a2ce79281da23a17a
Kaze Wong <kazewong.physics@gmail.com>
2025-01-06 12:56:18 -0500

    Move add food to component

--- src/routes/nutrition/+page.svelte
+++ src/routes/nutrition/+page.svelte
@@ -1,26 +1,7 @@
+<script>
+    import AddFood from "./AddFood.svelte";
+</script>
+
 <h1>Track nutrition and metrics here</h1>
 
-
-<ul>
-    <li class="py-2">
-        <input class='input input-bordered' type='text' placeholder='Name'>
-    </li>
-    <li class="pt-2">
-        <input class='input input-bordered' type='text' placeholder='Portion'>
-    </li>
-    <li class="">
-        <input class='input input-bordered' type='text' placeholder='Calories'>
-    </li>
-    <li class="">
-        <input class='input input-bordered' type='text' placeholder='Carb'>
-    </li>
-    <li class="">
-        <input class='input input-bordered' type='text' placeholder='Protein'>
-    </li>
-    <li class="pb-2">
-        <input class='input input-bordered' type='text' placeholder='Fat'>
-    </li>
-
-</ul>
-
-<button class='btn'>Add food</button>
+<AddFood />
